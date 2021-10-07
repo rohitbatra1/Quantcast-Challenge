158e4e98fad897a800fde986ef72ba02534e6c1f
Rohit Batra <rb4jx@virginia.edu>
2021-10-07 17:58:14 -0400

    Initial Commit
    Created Python file

---
+++ most_active_cookie.py
@@ -0,0 +1,22 @@
+
+
+# We define the most active cookie as one seen in the log the most times during a given day.
+
+# Most Active Cookie
+
+# Given a cookie log file in the following format:
+
+# cookie,timestamp
+# AtY0laUfhglK3lC7,2018-12-09T14:19:00+00:00
+# SAZuXPGUrfbcn5UA,2018-12-09T10:13:00+00:00
+# 5UAVanZf6UtGyKVS,2018-12-09T07:25:00+00:00
+# AtY0laUfhglK3lC7,2018-12-09T06:19:00+00:00
+# SAZuXPGUrfbcn5UA,2018-12-08T22:03:00+00:00
+# 4sMM2LxV07bPJzwf,2018-12-08T21:30:00+00:00
+# fbcn5UAVanZf6UtG,2018-12-08T09:30:00+00:00
+# 4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00
+
+# Write a command line program in your preferred language to process the log file and return the most active cookie for specified day. The example below shows how we'll execute your program.
+
+
+
